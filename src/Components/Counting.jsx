5327d454e863b7b01cd66c9a2685ae7be175a481
Chronowork1 <jianxie6464@gmail.com>
2019-10-29 14:51:15 -0700

    Completed timer

--- src/Components/Counting.jsx
+++ src/Components/Counting.jsx
@@ -3,66 +3,86 @@ import React from 'react';
 class Countup extends React.Component {
   constructor(props){
       super(props);
-      this.state={
+      this.state ={
         timerOn: false,
-        timeStart: 0,
+        timerStart: 0,
         timerTime: 0
       }
   }
 
-  startTime = () => {
+  startTimer = () => {
     this.setState({
-        timerOn: true,
-        timerTime: this.state.timerTime,
-        timeStart: Date.now() - this.state.timerTime
+      timerOn: true,
+      timerTime: this.state.timerTime,
+      timerStart: Date.now() - this.state.timerTime
     });
     this.timer = setInterval(() => {
-        this.setState({
-            timerTime: Date.now() - this.state.timerStart
-        })
+      this.setState({
+        timerTime: Date.now() - this.state.timerStart
+      });
     }, 10);
-  }
+  };
 
-  stopTimer = () => {
-    this.setState({
-        timerOn: false
-    });
-    clearInterval(this.timer);
-  }
+stopTimer = () => {
+  this.setState({ timerOn: false });
+  clearInterval(this.timer);
+};
+resetTimer = () => {
+  this.setState({
+    timerStart: 0,
+    timerTime: 0
+  });
+};
 
-
-
+  adjustTimer = input => {
+    const { timerTime, timerOn } = this.state;
+    const max = 216000000;
+    if (!timerOn) {
+      if (input === "incHours" && timerTime + 3600000 < max) {
+        this.setState({ timerTime: timerTime + 3600000 });
+      } else if (input === "decHours" && timerTime - 3600000 >= 0) {
+        this.setState({ timerTime: timerTime - 3600000 });
+      } else if (input === "incMinutes" && timerTime + 60000 < max) {
+        this.setState({ timerTime: timerTime + 60000 });
+      } else if (input === "decMinutes" && timerTime - 60000 >= 0) {
+        this.setState({ timerTime: timerTime - 60000 });
+      } else if (input === "incSeconds" && timerTime + 1000 < max) {
+        this.setState({ timerTime: timerTime + 1000 });
+      } else if (input === "decSeconds" && timerTime - 1000 >= 0) {
+        this.setState({ timerTime: timerTime - 1000 });
+      }
+    }
+  };
   render(){
-
-    const {timerTime} = this.state;
+    const { timerTime } = this.state;
     let centiseconds = ("0" + (Math.floor(timerTime / 10) % 100)).slice(-2);
     let seconds = ("0" + (Math.floor(timerTime / 1000) % 60)).slice(-2);
     let minutes = ("0" + (Math.floor(timerTime / 60000) % 60)).slice(-2);
     let hours = ("0" + Math.floor(timerTime / 3600000)).slice(-2);
-
     return(
-      <div>
-        <div>{hours}:{minutes}:{seconds}:{centiseconds}</div>
-        {this.state.timerOn === false && this.state.timerTime === 0
-        && (
-            <button onClick={this.startTime}>Start</button>
-        )}
+      <div className="stopWatch">
+        <div className="stopwatch-header">Stopwatch</div>
+        <div className="Stopwatch-display">{hours} : {minutes} : {seconds} : {centiseconds}</div>
 
+        {this.state.timerOn === false && this.state.timerTime === 0 && (
+        <button onClick={this.startTimer}>Start</button>
+        )}
         {this.state.timerOn === true && (
-            <button onClick={this.stopTime}>Stop</button>
+        <button onClick={this.stopTimer}>Stop</button>
         )}
-
         {this.state.timerOn === false && this.state.timerTime > 0 && (
-            <button onClick = {this.startTime}>Resume</button>
+        <button onClick={this.startTimer}>Resume</button>
         )}
-
         {this.state.timerOn === false && this.state.timerTime > 0 && (
-            <button onclick = {this.resetTime}>Reset</button>
+        <button onClick={this.resetTimer}>Reset</button>
         )}
+        
+
       </div>
+        
       )
   }
 
-}
+};
 
 export default Countup;
