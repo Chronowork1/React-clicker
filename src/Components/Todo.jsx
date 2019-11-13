af0d7a40a6f72323db957d162113b79e54260b64
Chronowork1 <jianxie6464@gmail.com>
2019-11-13 15:33:15 -0800

    Finish todo list at least for now

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -7,19 +7,38 @@ class Todo extends React.Component{
       inputValue: ''
     }
   }
-  
-divInput = (evt) =>{
-  this.setState({
-    inputValue: evt.target.value
-  })
-}
+
+
+  handleSubmit = (event) =>{
+    event.preventDefault(); // prevent default action of the form
+    const data = this.state
+    console.log("Final data is", data)
+  }
+
+  handleInputChange = (event) =>{
+    event.preventDefault();
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  componentDidMount(){
+    this.setState({
+      inputValue: "Kevin"
+    })
+  }
 
   render(){
+    const {inputValue} = this.state
+
     return(
       <div>
         <h1>Todo List</h1>
-        <div>{this.state.inputValue}</div>
-        <input value = {this.state.value} onChange= {evt =>{this.divInput(evt)}}/>
+        <form onSubmit = {this.handleSubmit}>
+          <div>{inputValue}</div>
+          <input type="text" placeholder="Your Name" name ="inputValue" onChange = {this.handleInputChange} />
+          <button>Submit</button>
+        </form>
       </div>
     )
   }
