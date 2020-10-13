89c06a116a50befe5cf9a4fd54a322764c502570
Chronowork1 <jianxie6464@gmail.com>
2020-10-13 01:19:10 -0700

    Updates

---
+++ precache-manifest.04e7a0cff7863102cb7eddc82b55b372.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fac335090c8336cd4e4ce88be46ee446",
+    "url": "/React-clicker/index.html"
+  },
+  {
+    "revision": "80e78980e637b1c605b5",
+    "url": "/React-clicker/static/css/main.f75c0975.chunk.css"
+  },
+  {
+    "revision": "ea5fb674bba146c0e6b1",
+    "url": "/React-clicker/static/js/2.5b3e64eb.chunk.js"
+  },
+  {
+    "revision": "80e78980e637b1c605b5",
+    "url": "/React-clicker/static/js/main.74f370f0.chunk.js"
+  },
+  {
+    "revision": "fa6c6fed3aebdb201936",
+    "url": "/React-clicker/static/js/runtime-main.70f09d2d.js"
+  }
+]);
